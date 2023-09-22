40dee11dcb427e616c7b401fb676fd11ebde5772
Eunsoo Ko <rhdmstn202@gmail.com>
2023-09-22 21:00:26 +0900

    commit: add login/register pages

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const Jeosooji = require("./models/jeosooji");
-const router = express.Router();
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const catchAsync = require("./utils/catchAsync");
+const User = require("./models/user");
 
 mongoose.connect("mongodb://localhost:27017/jeosooji", {
   useNewUrlParser: true,
@@ -22,7 +27,33 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const sessionConfig = {
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -32,14 +63,42 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "login",
+  }),
+  (req, res) => {
+    req.flash("success", "로그인");
+    res.redirect("/admin");
+  }
+);
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.get("/admin", (req, res) => {
   // res.redirect("/login");
   res.render("admin");
 });
 
-app.post("/", async (req, res) => {
-  res.send(req.body);
-});
+app.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "회원가입 완료");
+      res.redirect("/admin");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 app.listen(3000, () => {
   console.log("serving on port 3000");
