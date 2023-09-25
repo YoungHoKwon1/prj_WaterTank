dff6f511139e52ee1a75f1436e056848dbe01a9c
Eunsoo Ko <rhdmstn202@gmail.com>
2023-09-25 19:20:59 +0900

    commit: add authentication

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const Jeosooji = require("./models/jeosooji");
 const session = require("express-session");
 const flash = require("connect-flash");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const catchAsync = require("./utils/catchAsync");
@@ -27,6 +28,10 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("__method"));
+app.use(express.static(path.join(__dirname, "public")));
+
 const sessionConfig = {
   secret: "secret",
   resave: false,
@@ -38,26 +43,57 @@ const sessionConfig = {
   },
 };
 
-app.use(express.urlencoded({ extended: true }));
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/admin", (req, res) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "관리자 로그인 필요");
+    return res.redirect("/login");
+  }
+  res.render("admin");
+});
+
+app.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "회원가입 완료");
+        res.redirect("/admin");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 app.get("/login", (req, res) => {
   res.render("login");
@@ -67,38 +103,22 @@ app.post(
   "/login",
   passport.authenticate("local", {
     failureFlash: true,
-    failureRedirect: "login",
-  }),
-  (req, res) => {
-    req.flash("success", "로그인");
-    res.redirect("/admin");
-  }
-);
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.get("/admin", (req, res) => {
-  // res.redirect("/login");
-  res.render("admin");
-});
-
-app.post(
-  "/register",
-  catchAsync(async (req, res) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.flash("success", "회원가입 완료");
-      res.redirect("/admin");
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
+    failureRedirect: "/login",
+    successFlash: true,
+    successRedirect: "/admin",
   })
 );
+
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      req.flash("error", "로그아웃 중 오류가 발생했습니다.");
+      return res.redirect("/");
+    }
+    req.flash("success", "로그아웃");
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => {
   console.log("serving on port 3000");
